1c5b67b4392d5588396510f057a9ea0de2cf5028
tahakanar <tahakanar@gmail.com>
2022-01-18 00:27:13 +0300

    nextjs

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image='https://via.placeholder.com/400x300'
+      alt='Meet up'
+      title='First Meet up'
+      address='adana İstanbul 01169 sk'
+      description='deneme1-1-2'
+    />
+  );
+}
+
+export default MeetupDetails;
